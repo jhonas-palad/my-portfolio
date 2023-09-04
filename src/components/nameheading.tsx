1a6775b24530a766398397685c07fb402f587176
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-09-04 22:27:26 +0800

    first section almost done

--- src/components/nameheading.tsx
+++ src/components/nameheading.tsx
@@ -4,9 +4,9 @@ const NameHeading = () => {
   return (
     
     <h1
-      className="z-20 mt-5 font-bold text-5xl text-left text-primary-900"
+      className="z-20 dark:text-white text-secondary-900 mt-5 font-bold text-5xl text-left"
     >
-    Jhonas Palad<span className='dark:text-white text-secondary-900'>.</span>
+    Jhonas<br/>Palad<span className='text-primary-900'>.</span>
     </h1>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,37 +1,54 @@
 import React from 'react'
 import Navbar from '../components/navbar'
-import myPic from '../assets/my-pic-final.png'
+import myPic from '../assets/my-image.png'
 import '../assets/css/home.css'
 import {FaFacebook, FaInstagram, FaGithub, FaLinkedin}from 'react-icons/fa'
 import NameHeading from '../components/nameheading'
 const HomePage:React.FC = () => {
   return (
     <>
-    <div className='w-full h-[100vh]'>
-      <div className='first'>
-        <div className='my-profile'>
-          <div className='my-pic relative flex justify-center items-center'>
-            <img id="my-image" src={myPic}/>
-            <div className='box-decor'/>
-            <div className='box-decor'/>
-            <div className='box-decor'/>
-            <div className='box-decor'/>
-            <div className='box-decor'/>
-          </div>
-          <div className='flex flex-col gap-8'>
-            <div className='my-name z-40'>
-              <NameHeading/>
-              <div className='line-x'/>
-            </div>
-              {/* <img className="object-contain" src={myPic}/> */}
-            <div className='my-links'>
+    <div className='w-full'>
+      <div className='section first'>
+        <div className='border-0 flex justify-center items-center'>
+          <div className='my-profile max-w-lg'>
+            <div className='my-links mr-6'>
               <FaInstagram/>
               <FaFacebook/>
               <FaLinkedin/>
               <FaGithub/>
             </div>
+            <div className='my-name w-full z-40 mb-3'>
+                <h1>
+                Jhonas<br/>Emmanuel<br/>Palad<span className='text-primary-900'>.</span>
+                </h1>
+                <div className='line-x'/>
+              </div>
+            <div className='my-pic'>
+                <div className='relative flex justify-center'>
+                  <img id="my-image" src={myPic}/>
+                  <div className='box-decor'/>
+                </div>
+            </div>
+                {/* <img className="object-contain" src={myPic}/> */}
+    
           </div>
-          
+        </div>
+        <div className='my-description'>
+          <p className='text-left'>
+            - Profile
+          </p>
+          <h1>
+            Programmer and Developer, based in Philippines.
+          </h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+          <div className='my-links mr-6'>
+              <FaInstagram/>
+              <FaFacebook/>
+              <FaLinkedin/>
+              <FaGithub/>
+            </div>
         </div>
       </div>
     </div>
